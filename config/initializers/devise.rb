389d6e042aa6e2bbd92589c58f9e1d2af0eeaa46
pedorgrande <pete@thecoderfactory.com>
2015-10-03 18:46:40 +1000

    zapier

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -236,11 +236,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, '769427878227-o7fck6sk0sipv0bhec4f2tbmns4nvjk1.apps.googleusercontent.com',
-    'r7T94pwjkkv8vKUmUKYN1z1H',
-    { access_type: "offline", approval_prompt: "",
-      scope: 'userinfo.email,calendar' }
+  # require 'omniauth-google-oauth2'
+  # config.omniauth :google_oauth2, '769427878227-o7fck6sk0sipv0bhec4f2tbmns4nvjk1.apps.googleusercontent.com',
+  #   'r7T94pwjkkv8vKUmUKYN1z1H',
+  #   { access_type: "offline", approval_prompt: "",
+  #     scope: 'userinfo.email,calendar' }
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
