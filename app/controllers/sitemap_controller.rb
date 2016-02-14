0c5e686ae9c0d368a7bf353e991f35dce815346a
pedorgrande <pete@thecoderfactory.com>
2016-02-15 00:29:59 +1100

    update paths

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -7,14 +7,15 @@ class SitemapController < ApplicationController
     @courses = Course.active
 	  @posts = Post.all
     @static_pages = [
-      		  	root_url, 
-      		  	blog_url, 
-      		  	coder_factory_workshops_for_beginners_url,
-      		  	coder_factory_part_time_courses_url,
+      		  	root_url,
+      		  	blog_url,
+      		  	learn_to_code_url,
+							coding_courses_url,
+      		  	courses_web_app_builder_url,
       		  	coding_workshops_for_businesses_url,
-      		  	fast_track_intensive_bootcamp_url,
-      		  	fast_track_url,
-      		  	coding_for_schools_url,
+      		  	# fast_track_intensive_bootcamp_url,
+      		  	# fast_track_url,
+      		  	# coding_for_schools_url,
       		  	community_url,
       		  	about_url,
       		  	contact_url,
